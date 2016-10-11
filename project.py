632227905ec2e36ea6f73e83a4acc091e6c19495
Ruslan Mursalzade <ruslan.mursal@gmail.com>
2016-10-10 18:11:28 -0700

    Udacity Catalog Project

---
+++ project.py
@@ -0,0 +1,337 @@
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, Item, User
+from flask import session as login_session
+import random
+import string
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+import datetime
+
+app = Flask(__name__)
+
+# Connect to Database and create database session
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+# Create anti-forgery state token
+
+
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    # return "The current session state is %s" % login_session['state']
+    return render_template('login.html', STATE=state)
+
+
+@app.route('/fbconnect', methods=['POST'])
+def fbconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    access_token = request.data
+    print "access token received %s " % access_token
+
+    app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
+        'web']['app_id']
+    app_secret = json.loads(
+        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
+        app_id, app_secret, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+
+    # Use token to get user info from API
+    userinfo_url = "https://graph.facebook.com/v2.4/me"
+    # strip expire tag from access token
+    token = result.split("&")[0]
+
+    url = 'https://graph.facebook.com/v2.4/me?%s&fields=name,id,email' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    # print "url sent for API access:%s"% url
+    # print "API JSON result: %s" % result
+    data = json.loads(result)
+    login_session['provider'] = 'facebook'
+    login_session['username'] = data["name"]
+    login_session['email'] = data["email"]
+    login_session['facebook_id'] = data["id"]
+
+    # The token must be stored in the login_session in order to properly
+    # logout, let's strip out the information before the equals sign in our
+    # token
+    stored_token = token.split("=")[1]
+    login_session['access_token'] = stored_token
+
+    # Get user picture
+    url = 'https://graph.facebook.com/v2.4/me/picture?%s&redirect=0&height=200&width=200' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    data = json.loads(result)
+
+    login_session['picture'] = data["data"]["url"]
+
+    # see if user exists
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+
+    flash("Now logged in as %s" % login_session['username'])
+    return output
+
+
+@app.route('/fbdisconnect')
+def fbdisconnect():
+    facebook_id = login_session['facebook_id']
+    # The access token must me included to successfully logout
+    access_token = login_session['access_token']
+    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (
+        facebook_id, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'DELETE')[1]
+    return "you have been logged out"
+
+# Disconnect based on provider
+
+
+@app.route('/disconnect')
+def disconnect():
+    if 'provider' in login_session:
+        if login_session['provider'] == 'facebook':
+            fbdisconnect()
+            del login_session['facebook_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        del login_session['user_id']
+        del login_session['provider']
+        flash("You have successfully been logged out.")
+        return redirect(url_for('showCatalog'))
+    else:
+        flash("You were not logged in")
+        return redirect(url_for('showCatalog'))
+
+# User Helper Functions
+
+
+def createUser(login_session):
+    newUser = User(name=login_session['username'], email=login_session[
+                   'email'], picture=login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
+
+
+# JSON APIs to view Restaurant Information
+@app.route('/catalog')
+def catalogJSON():
+    categories = session.query(Category).all()
+    items = session.query(Item).all()
+    return jsonify(
+        Categories=[
+            category.serialize for category in categories], Items=[
+            item.serialize for item in items])
+
+# Show all categories and latest items
+
+
+@app.route('/')
+def showCatalog():
+    categories = session.query(Category).order_by(asc(Category.name))
+    latestItems = session.query(Item).order_by(Item.created.desc())
+    return render_template(
+        'catalog.html',
+        categories=categories,
+        latestItems=latestItems)
+
+# Show all items in a specific category
+
+
+@app.route('/catalog/<int:category_id>/items')
+def showItems(category_id):
+    category = session.query(Category).filter_by(id=category_id).one()
+#    creator = getUserInfo(category.user_id)
+    items = session.query(Item).filter_by(
+        category_id=category_id).all()
+    return render_template('items.html', items=items, category=category)
+
+
+# Show an items's details
+@app.route('/catalog/<int:category_id>/<int:item_id>')
+def itemDetails(category_id, item_id):
+    category = session.query(Category).filter_by(id=category_id).one()
+#    creator = getUserInfo(category.user_id)
+    item = session.query(Item).filter_by(id=item_id).one()
+    return render_template('item.html', item=item, category=category)
+
+# Add a new item
+
+
+@app.route('/catalog/items/new', methods=['GET', 'POST'])
+def newItem():
+    if 'username' not in login_session:
+        return redirect('/login')
+    categories = session.query(Category).order_by(asc(Category.name))
+    user_id = login_session['user_id']
+    if request.method == 'POST':
+        category = session.query(Category).filter_by(
+            name=request.form['category']).one()
+        newItem = Item(
+            name=request.form['name'],
+            description=request.form['description'],
+            price=request.form['price'],
+            category_id=category.id,
+            created=datetime.datetime.now(),
+            user_id=user_id)
+        session.add(newItem)
+        session.commit()
+        flash('New Item -  %s  Successfully Created' % (newItem.name))
+        return redirect(url_for('showCatalog'))
+    else:
+        return render_template('newitem.html', categories=categories)
+
+# Edit an item
+
+
+@app.route('/catalog/<int:item_id>/edit', methods=['GET', 'POST'])
+def editItem(item_id):
+    categories = session.query(Category).order_by(asc(Category.name))
+    item = session.query(Item).filter_by(id=item_id).one()
+    if 'username' not in login_session:
+        return redirect('/login')
+    if item.user_id != login_session['user_id']:
+        return "<script>function myFunction() {alert('You are not authorized to edit other users' items.');}</script><body onload='myFunction()''>"
+    if request.method == 'POST':
+        if request.form['name']:
+            category = session.query(Category).filter_by(
+                name=request.form['category']).one()
+            item.name = request.form['name']
+            item.description = request.form['description']
+            item.price = request.form['price']
+
+            item.category_id = category.id
+            flash('%s Successfully Edited' % item.name)
+            return redirect(url_for('showItems', category_id=item.category.id))
+    else:
+        return render_template(
+            'edititem.html',
+            categories=categories,
+            item=item)
+
+
+# Delete an item
+@app.route('/catalog/<int:item_id>/delete', methods=['GET', 'POST'])
+def deleteItem(item_id):
+    item = session.query(Item).filter_by(id=item_id).one()
+    if 'username' not in login_session:
+        return redirect('/login')
+    if item.user_id != login_session['user_id']:
+        return "<script>function myFunction() {alert('You are not authorized to delete other users' items.');}</script><body onload='myFunction()''>"
+    if request.method == 'POST':
+        session.delete(item)
+        flash('%s Successfully Deleted' % item.name)
+        session.commit()
+        return redirect(url_for('showItems', category_id=item.category.id))
+    else:
+        return render_template('deleteitem.html', item=item)
+
+# Add a new category
+
+
+@app.route('/catalog/new', methods=['GET', 'POST'])
+def newCategory():
+    if 'username' not in login_session:
+        return redirect('/login')
+    user_id = login_session['user_id']
+    if request.method == 'POST':
+        newCategory = Category(name=request.form['name'], user_id=user_id)
+        session.add(newCategory)
+        session.commit()
+        flash('New Category %s Successfully Created' % (newCategory.name))
+        return redirect(url_for('showCatalog'))
+    else:
+        return render_template('newcategory.html')
+
+
+# Edit a category
+@app.route('/catalog/category/<int:category_id>/edit', methods=['GET', 'POST'])
+def editCategory(category_id):
+    category = session.query(Category).filter_by(id=category_id).one()
+    if 'username' not in login_session:
+        return redirect('/login')
+    if category.user_id != login_session['user_id']:
+        return "<script>function myFunction() {alert('You are not authorized to edit other users' categories.');}</script><body onload='myFunction()''>"
+    if request.method == 'POST':
+        if request.form['name']:
+            category.name = request.form['name']
+            flash('%s Successfully Edited' % category.name)
+            return redirect(url_for('showItems', category_id=category_id))
+    else:
+        return render_template('editCategory.html', category=category)
+
+
+# Delete a category and all of the items associated with it
+@app.route(
+    '/catalog/category/<int:category_id>/delete',
+    methods=[
+        'GET',
+        'POST'])
+def deleteCategory(category_id):
+    category = session.query(Category).filter_by(id=category_id).one()
+    items = session.query(Item).filter_by(category_id=category.id).all()
+    if 'username' not in login_session:
+        return redirect('/login')
+    if category.user_id != login_session['user_id']:
+        return "<script>function myFunction() {alert('You are not authorized to delete other users' categories.');}</script><body onload='myFunction()''>"
+    if request.method == 'POST':
+        for item in items:
+            session.delete(item)
+        session.delete(category)
+        flash(
+            '%s Category and All Items Associated with this Category Successfully Deleted' %
+            category.name)
+        session.commit()
+        return redirect(url_for('showCatalog'))
+    else:
+        return render_template('deleteCategory.html', category=category)
+
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
---
+++ readme.md
@@ -0,0 +1,26 @@
+#Udacity Catalog Project
+
+This is a simple catalog app built with Python, Flask and SQLAlchemy. The app supports user authentication and authorization using oAuth 2.0 and Facebook Login API.
+Logged in users can add categories and items, as well as edit, delete categories and items that they have created. Users who are not logged in can view the contents of the catalog but cannot make changes.
+
+####Installation
+
+1. Ensure you have git installed. To install on Mac, downdload and install from: http://git-scm.com/download/mac. To install on Windows, downdload and install from: http://git-scm.com/download/win.
+2. Ensure you have Python installed. To install on Mac, downdload and install from: https://www.python.org/ftp/python/2.7.12/python-2.7.12-macosx10.6.pkg. To install on Windows, downdload and install from: https://www.python.org/ftp/python/2.7.12/python-2.7.12.msi.
+3. Download and install Virtual Box from https://www.virtualbox.org/wiki/Downloads.
+4. Download and install Vagrant from https://www.vagrantup.com/downloads.
+5. Open system's command line.
+6. Change to the desired directory
+  - Example: `cd ~/` for the main User 
+3. Using Git, clone the Udacity VM configuration:
+  - Run: `git clone https://github.com/udacity/fullstack-nanodegree-vm.git`
+  - This will create a new directory titled *vagrant* that contains all of the necessary configurations to run this application.
+4. Move to the *vagrant* folder by entering: `cd ~/vagrant/`
+5. Clone the github repository by running the command below:
+  - Run: `git clone https://github.com/ruslanml/Udacity-Catalog-Project.git`
+  - This will create a directory inside the *vagrant* directory titled *catalog*.
+6. Run Vagrant by entering: `vagrant up`
+7. Log into Vagrant VM by entering: `vagrant ssh`
+8. Move to *catalog* directory by entering: `cd /vagrant/catalog/`
+9. Run the main project file, run: `python project.py`
+10. View the app on your browser, go to: `http://0.0.0.0:5000/`
